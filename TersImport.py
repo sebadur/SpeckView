5468c6d91e1618194d5b72ba3ac12acf5e8cee61
jalmar <jalmar@jalmar-Laptop>
2016-02-11 16:08:18 +0100

    File-Plugin für TERS-Files

---
+++ TersImport.py
@@ -0,0 +1,38 @@
+import gwy
+import sys
+
+plugin_type = "FILE"
+plugin_desc = "Importing Ters-Files (.ters)"
+
+def detect_by_name(filename):
+    if (filename.endswith(".ters")):
+        return 100
+    else:
+        return 0
+
+def detect_by_content(filename, head, tail, filesize):
+    return 100
+
+def load(filename, mode=None):
+
+    c = gwy.Container()
+    d = gwy.DataField(100, 100, 100, 100, 1)
+    for i in range(100):
+        for j in range(100):
+            d.set_val(i, j, i) # draws linear gradient
+    c.set_object_by_name("/0/data", d)
+    return c
+
+def save(data, filename, mode=None):
+    f = open(filename, "w")
+    datafield_num = 1
+    for key in data.keys():
+        if isinstance(data.get_object(key), gwy.DataField):
+            d = data.get_object(key)
+            f.write("Datafield "+ str(datafield_num) + '\n')
+            datafield_num += 1
+            for row in range(d.get_yres()):
+                for col in range(d.get_xres()):
+                    f.write(str(d.get_val(col, row))+'\n')
+                f.close()
+    return True
